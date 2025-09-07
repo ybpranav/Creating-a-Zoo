5abe674710f72f5c4feb2c1f8e86fb781ec7053d
pranavyb <pranavshetty99@gmail.com>
2025-09-07 18:59:29 +0530

    updated main

--- src/Penguin.java
+++ src/Penguin.java
@@ -1,11 +1,39 @@
-public class Penguin extends Animal{
+public class Penguin extends Animal implements Walk,Swim{
+
+    private int speed;
+
+    public int getSwimming() {
+        return swimming;
+    }
+
+    public void setSwimming(int swimming) {
+        this.swimming = swimming;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    private int swimming;
     @Override
     public void eatingCompleted() {
-        System.out.println("Dolphin: I am eating delicious fish");
+        System.out.println("Penguin: I am eating delicious fish");
     }
 
     public Penguin(String nameOfAnimal) {
         super(nameOfAnimal);
     }
 
+    @Override
+
+    public void walking(){
+
+    }
+    public void swimming(){
+
+    }
 }
